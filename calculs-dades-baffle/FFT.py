0faef6fedf7be78ac366f0793cec437d9380e809
Eloi-Estevez <168549406+Eloi-Estevez@users.noreply.github.com>
2025-07-29 11:42:03 +0200

    Update FFT.py

--- calculs-dades-baffle/FFT.py
+++ calculs-dades-baffle/FFT.py
@@ -3,29 +3,29 @@ import pandas as pd
 import cdm_bindings
 from pycdm import PyCDM
 import time
-import scipy
 import queue
 import signal
+import sys
 
-cdm = PyCDM("172.16.17.25")
-dq = cdm.reg_msg("DATA.FRAME")
+cdm = PyCDM("172.16.7.210")
+dq = cdm.reg_msg("DATA.FRAME.L1")
 
 def empty_queue(dq,buffer, chanels = range(20)):
     while (True):
-            try:
-                data = dq.get(block=False, timeout=0.01)[1]
-                print(1)
-            except queue.Empty:
-                #print("Empty message")
-                break
-            decoder = cdm_bindings.FrameDecoder(data)
-            header = decoder.get_header()
-            for i in range(header.count):
-                f = decoder.read_sumsq_frame()
-                buffer[20].append(f.timestamp)
-                for i in chanels:
-                    buffer[i].append(f.data[i].sum/f.count)
-                    buffer[i] = buffer[i][-2048:]       
+        try:
+            data = dq.get(block=True, timeout=0.01)[1]
+        except queue.Empty:
+            #print("Empty message")
+            break
+        decoder = cdm_bindings.FrameDecoder(data)
+        header = decoder.get_header()
+        for i in range(header.count):
+            f = decoder.read_sumsq_frame()
+            buffer[20].append(f.timestamp)
+            buffer[20] = buffer[20][-2048:]
+            for i in chanels:
+                buffer[i].append(f.data[i].sum/f.count)
+                buffer[i] = buffer[i][-2048:] 
     return buffer
 
 def computefft(buffer, freq, result, chanels=range(20)):
@@ -39,8 +39,18 @@ def computefft(buffer, freq, result, chanels=range(20)):
 
 def save_file(result, chanels=range(20)):
     for i in chanels:
-        result[i].to_csv(time.strftime('%Y%m%d%H%M%S',time.gmtime())+f'-fft-{i}')
+        result[i].to_csv('./Output/'+time.strftime('%Y%m%d%H%M%S',time.gmtime())+f'-fft-{i}.csv')
     
+
+def signal_handler(signum, frame):
+    signal.signal(signum, signal.SIG_IGN)
+    print('Captured Ctrl-C, saving data...')
+    save_file(result) 
+    cdm.close()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler) 
+
 
 x = np.fft.rfftfreq(2048)
 buffer = [[] for i in range(21)]
@@ -49,26 +59,23 @@ first = True
 
 
 while True:
-    try:
+    #try:
         buffer = empty_queue(dq,buffer)#, chanels = [0,1])
         if first and len(buffer[0])== 2048:
             diff = np.diff(buffer[20])
             timestep = np.mean(diff)
-            freq = np.fft.rfftfreq(2048,timestep/1.e6)
-            refactor_time = time.time() - buffer[20][-1]/1.e6
+            freq = np.fft.rfftfreq(2048,timestep/1e3)
+            refactor_time = time.time() - buffer[20][-1]/1e3
             result = computefft(buffer, freq, result)#, chanels = [0,1])
             first= False
             #print(result)
-            print('Computed fft')
+            #print('Computed fft')
         elif not first and time.time()-result[0]['timestamp'].iloc[-1] > 5 :
-            ts = refactor_time + buffer[20]/1.e6
+            ts = [refactor_time + x/1e3 for x in buffer[20]]
             if time.time() - ts[-1] > 0.1: raise Exception("Old data") 
             result = computefft(buffer, freq, result)#, chanels = [0,1])
             print('Computed fft')
             #print(result)
-    except KeyboardInterrupt:
-        print("Captured Ctrl+C")
-        save_file(result)#,chanels=[0,1])
-        break
+    
 
 cdm.close()
